9b00fe87e147bfb128a59d9a5531ee3f066c5839
Gionatan Lombardi <gionatan.lombardi@musement.com>
2021-03-02 11:23:05 +0100

    feat: added Counter component with custom hook

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import LogoImg from './components/LogoImg';
-import Product from './components/Product'
+import Product from './components/Product';
+import Counter from './components/Counter';
 import './App.css';
 
 function App() {
@@ -9,26 +10,17 @@ function App() {
     <div className="App">
       <header className="App-header">
         <LogoImg alternativeText="React Header Logo" imageSrc={logo} />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
       <main>
+        <Counter modalIsOpen={true} number={234} />
+        <Counter modalIsOpen={false} number={32} />
         <ul>
          <Product name="Red Shoe" price={23.5} />
          <Product name="Blue Shoe" price={98.3} />
          <Product name="Black Shoe" price={52.7} />
         </ul>
       </main>
-      <footer><LogoImg alternativeText="Edgemony Footer Logo" imageSrc="https://edgemony.com/wp-content/uploads/2020/03/cropped-Logo-edgemony_TeBIANCO-04.png" /></footer>
+      <footer className="App-footer"><LogoImg alternativeText="Edgemony Footer Logo" imageSrc="https://edgemony.com/wp-content/uploads/2020/03/cropped-Logo-edgemony_TeBIANCO-04.png" /> © { (new Date().getFullYear())}</footer>
     </div>
   );
 }
